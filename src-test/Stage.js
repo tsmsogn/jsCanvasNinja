48626f2686076771ebb3dbccf8e37b5a877ea03e
tsmsogn <tsmsogn@gmail.com>
2013-02-12 10:02:17 +0900

    Modified test cases

--- src-test/Stage.js
+++ src-test/Stage.js
@@ -1,7 +1,15 @@
 TestCase('StageTestCase', {
-    setUp:function () {
+    'setUp':function () {
         stage = new jsCanvasNinja.Stage();
     },
-    tearDown:function () {
+    'tearDown':function () {
+    },
+    'testMode':function () {
+        stage.setMode('foo');
+        assertEquals('', 'foo', stage.getMode());
+        stage.setMode(0);
+        assertEquals('', 'select', stage.getMode());
+        stage.setMode(1);
+        assertEquals('', 'insert', stage.getMode());
     }
 });
