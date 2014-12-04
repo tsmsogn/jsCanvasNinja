2a0e95be7a415177e81f13c79e570347efce96f5
tsmsogn <tsmsogn@gmail.com>
2014-12-04 18:19:34 +0900

    Modify task(s) executed with `grunt test`

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,6 +54,6 @@ module.exports = function(grunt) {
   grunt.registerTask('default', ['uglify']);
 
   // Test task(s).
-  grunt.registerTask('test', ['jshint']);
+  grunt.registerTask('test', ['jasmine']);
 
 };
