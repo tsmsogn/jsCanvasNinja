6261409c1c77dd9b121aa9ee891b49ba083cd3b4
tsmsogn <tsmsogn@gmail.com>
2014-12-15 16:34:31 +0900

    Oops...

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
     },
     jasmine: {
       all: {
-        src: 'src/*.js',
+        src: 'src/**/*.js',
         options: {
           specs: 'test/**/*Spec.js',
           vendor: [
